6ad6cdf78a2378254ff72a929821aee9d60d7994
Yuriy <batm1x1@gmail.com>
2023-02-03 11:37:29 +0300

    add ConfigModule for env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,16 @@
 import { Module } from '@nestjs/common';
 import {DishModule} from "src/dish/dish.module";
 import {MongooseModule} from "@nestjs/mongoose";
+import { ConfigModule } from '@nestjs/config';
 import {DrinkModule} from "src/drink/drink.module";
 
 @Module({
    imports: [
-      MongooseModule.forRoot('mongodb+srv://batm1x:bmDxd68y3kNaiPh@cluster0.mkweogl.mongodb.net/Cafe?retryWrites=true&w=majority'),
+      ConfigModule.forRoot({envFilePath: '.env', isGlobal: true}),
+      MongooseModule.forRoot(process.env.DB_URI),
       DishModule,
       DrinkModule
-   ]
+   ],
 })
 export class AppModule {}
+
